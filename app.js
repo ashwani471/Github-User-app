28af964966fdbafd3b0363a553bed95d6ee22410
Ashwani Kumar <ashwanikumar351999@gmail.com>
2023-03-04 18:52:01 +0530

    new things

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const github = new Github;
 
+const ui = new UI;
+
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup',(e)=>{
@@ -8,13 +10,17 @@ searchUser.addEventListener('keyup',(e)=>{
     if(userName!==''){
         github.getUser(userName)
         .then(data=>{
-            if(data.profile.message === 'Not found'){
+            if(data.profile.message === 'Not Found'){
                 //show alert
+                ui.showAlert('User Not Found' , 'alert alert-danger');
             }else{
                 //show user
+                console.log(data);
+                 ui.showUser(data.profile);
             }
         }); 
     }else{
         //clear profile
+        ui.clearProfile();
     }
 })
